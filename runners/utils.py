33a0f4e230a7190ea15c6c57ca25e216fa4cfa95
Hao Xin <xin11@purdue.edu>
2023-04-15 21:29:53 -0500

    Update utils.py

--- runners/utils.py
+++ runners/utils.py
@@ -1,1 +1,7 @@
-
+def get_model(config):
+    if config.data.dataset == 'CIFAR10' or config.data.dataset == 'CELEBA':
+        return NCSNv2(config).to(config.device)
+    elif config.data.dataset == "FFHQ":
+        return NCSNv2Deepest(config).to(config.device)
+    elif config.data.dataset == 'LSUN':
+        return NCSNv2Deeper(config).to(config.device)
