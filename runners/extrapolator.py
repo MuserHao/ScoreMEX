2df7961074311e90dd11f9d142383929a3cd6159
Hao Xin <xin11@purdue.edu>
2023-04-20 00:58:34 -0500

    Update extrapolator.py

--- runners/extrapolator.py
+++ runners/extrapolator.py
@@ -1,6 +1,70 @@
 import torch
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
+
+class Extrapolator:
+    def __init__(self, model, data_shape, x_mod, sigmas):
+        """
+        Initialize an extrapolator object with a regression model, data shape, and sigmas.
+
+        Parameters:
+        model (object): The regression model to use for extrapolation.
+        data_shape (list or tuple): The shape of the data points.
+        x_mod (torch tensor): the x-value score model will be used on.
+        sigmas (list): The standard deviations of diffusion (Gaussian) noise level.
+        """
+        self.model = model
+        self.data_shape = data_shape
+        self.x_mod = x_mod
+        self.sigmas = sigmas
+        self.labels = []
+        for c, sigma in enumerate(sigmas):
+            labels = torch.ones(self.data_shape[0], device=x_mod.device) * c
+            labels = labels.long()
+            self.labels += [labels]
+        self.x = None
+        self.y = None
+        
+    def generate_data(self, scorenet):
+        """
+        Generate training data using the specified scorenet.
+
+        Parameters:
+        data_generator (function): A function or model that generates training data.
+                                   Should take two arguments: data_shape and sigmas.
+                                   Should return two arrays, x and y.
+        """
+        #TODO
+        self.x, self.y = scorenet(x_mod, labels)
+        
+    def train_model(self):
+        """
+        Train the regression model using the generated training data.
+        Raises a ValueError if no training data has been generated.
+        """
+        if self.x is None or self.y is None:
+            raise ValueError("Training data not generated")
+        self.model.fit(self.x.reshape(-1, 1), self.y)
+        
+    def make_prediction(self, x_pred):
+        """
+        Use the trained regression model to make a prediction for the given x values.
+
+        Parameters:
+        x_pred (array-like): The x values for which to make predictions.
+
+        Returns:
+        array-like: The predicted y values.
+        Raises a ValueError if no training data has been generated.
+        """
+        if self.x is None or self.y is None:
+            raise ValueError("Training data not generated")
+        return self.model.predict(x_pred.reshape(-1, 1))
+
+
+
+      
 @torch.no_grad()
 def extrapolated_Langevin_dynamics(x_mod, scorenet, sigmas, n_steps_each=200, step_lr=0.000008,
                              final_only=False, verbose=False, denoise=True):
