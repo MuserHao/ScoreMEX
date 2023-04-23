f4f6b26d4c58a3b830e9dc51f8a69e32e827a4a6
Hao Xin <xin11@purdue.edu>
2023-04-23 15:09:48 -0500

    Update extrapolator.py

--- runners/extrapolator.py
+++ runners/extrapolator.py
@@ -39,9 +39,11 @@ class Extrapolator:
         """
         self.x = np.array([torch.flatten(x).numpy() for x in self.sigmas])
         self.y = []
-        for labels in self.labels:
+        sigma_min = self.sigmas[-1]
+        for labels, sigma in zip(self.labels, self.sigmas):
             score = scorenet(self.x_mod, labels)
             # might need to standardize score to fit the model better?
+            score *= (sigma / sigma_min) ** 2      # scaling by sigmas squares
             self.y += [score]
         self.y = np.array([torch.flatten(y).numpy() for y in self.y])
         
@@ -73,40 +75,34 @@ class Extrapolator:
 
 # still need to modify this
 @torch.no_grad()
-def extrapolated_Langevin_dynamics(x_mod, scorenet, sigmas, n_steps_each=200, step_lr=0.000008,
+def extrapolated_Langevin_dynamics(x_mod, scorenet, extrapolator, sigmas, n_steps=200, step_size=0.000008,
                              final_only=False, verbose=False, denoise=True):
     images = []
+    for s in range(n_steps_each):
+    grad = scorenet(x_mod, labels)
 
-    with torch.no_grad():
-        for c, sigma in enumerate(sigmas):
-            labels = torch.ones(x_mod.shape[0], device=x_mod.device) * c
-            labels = labels.long()
-            step_size = step_lr * (sigma / sigmas[-1]) ** 2
-            for s in range(n_steps_each):
-                grad = scorenet(x_mod, labels)
+    noise = torch.randn_like(x_mod)
+    grad_norm = torch.norm(grad.view(grad.shape[0], -1), dim=-1).mean()
+    noise_norm = torch.norm(noise.view(noise.shape[0], -1), dim=-1).mean()
+    x_mod = x_mod + step_size * grad + noise * np.sqrt(step_size * 2)
 
-                noise = torch.randn_like(x_mod)
-                grad_norm = torch.norm(grad.view(grad.shape[0], -1), dim=-1).mean()
-                noise_norm = torch.norm(noise.view(noise.shape[0], -1), dim=-1).mean()
-                x_mod = x_mod + step_size * grad + noise * np.sqrt(step_size * 2)
+    image_norm = torch.norm(x_mod.view(x_mod.shape[0], -1), dim=-1).mean()
+    snr = np.sqrt(step_size / 2.) * grad_norm / noise_norm
+    grad_mean_norm = torch.norm(grad.mean(dim=0).view(-1)) ** 2 * sigma ** 2
 
-                image_norm = torch.norm(x_mod.view(x_mod.shape[0], -1), dim=-1).mean()
-                snr = np.sqrt(step_size / 2.) * grad_norm / noise_norm
-                grad_mean_norm = torch.norm(grad.mean(dim=0).view(-1)) ** 2 * sigma ** 2
-
-                if not final_only:
-                    images.append(x_mod.to('cpu'))
-                if verbose:
-                    print("level: {}, step_size: {}, grad_norm: {}, image_norm: {}, snr: {}, grad_mean_norm: {}".format(
-                        c, step_size, grad_norm.item(), image_norm.item(), snr.item(), grad_mean_norm.item()))
-
-        if denoise:
-            last_noise = (len(sigmas) - 1) * torch.ones(x_mod.shape[0], device=x_mod.device)
-            last_noise = last_noise.long()
-            x_mod = x_mod + sigmas[-1] ** 2 * scorenet(x_mod, last_noise)
+        if not final_only:
             images.append(x_mod.to('cpu'))
+        if verbose:
+            print("level: {}, step_size: {}, grad_norm: {}, image_norm: {}, snr: {}, grad_mean_norm: {}".format(
+                c, step_size, grad_norm.item(), image_norm.item(), snr.item(), grad_mean_norm.item()))
 
-        if final_only:
-            return [x_mod.to('cpu')]
-        else:
-            return images
+    if denoise:
+        last_noise = (len(sigmas) - 1) * torch.ones(x_mod.shape[0], device=x_mod.device)
+        last_noise = last_noise.long()
+        x_mod = x_mod + sigmas[-1] ** 2 * scorenet(x_mod, last_noise)
+        images.append(x_mod.to('cpu'))
+
+    if final_only:
+        return [x_mod.to('cpu')]
+    else:
+        return images
